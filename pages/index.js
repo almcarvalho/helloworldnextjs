d1a2b4dd9c4e71dd860728ea673b3a7999c7e774
almcarvalho <antoniolucas@live.com>
2021-07-07 15:50:30 -0300

    primeira versão

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+    return <div> 
+        <h1> Hello World Next.js </h1> 
+        <br/> Parece simples mas até aqui aprendi muita coisa: colocar o projeto no ar, versionando no github e publicado no servidor de produção vercel <br/> 
+        Agradecimentos: 
+        </div>
+}
+
+export default Home
