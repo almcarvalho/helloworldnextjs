a069051fadd87ce5d83627860b036cb0c6f8eb8a
almcarvalho <antoniolucas@live.com>
2021-07-07 16:00:24 -0300

    alterações básicas na index

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home() {
         <h1> Hello World Next.js </h1> 
         <h1> Meu primeiro site usando uma das ferramentas mais modernas de desenvolvimento web. </h1> 
         <br/> Parece simples mas até aqui aprendi muita coisa: colocar o projeto no ar, versionando no github e publicado no servidor de produção vercel instantaneamente depois do commit na branch master <br/> 
-        Agradecimentos: <br/>
+        Agradecimentos:  <br/>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/EW7m2WIvFgQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
 }
