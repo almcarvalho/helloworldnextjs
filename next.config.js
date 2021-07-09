2983c62d0fc469f924845a36a5f6f8c295e9ad26
almcarvalho <antoniolucas@live.com>
2021-07-09 13:28:29 -0300

    atualizacao de seguranca

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    env: {
-        apiKey: 'AIzaSyD3Cfl96n7K9qd4ZHRBxM5NQAgk2iz9IzM',
-        authDomain: "hellonode-23652.firebaseapp.com",
-        projectId: "hellonode-23652"
-    },
-}
